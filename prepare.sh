b7c027c0d31ee74af8a91d30a2098231618a9449
Isaac <isaacmues@gmail.com>
2022-08-26 12:21:26 -0500

    Corrige error de dedo

--- prepare.sh
+++ prepare.sh
@@ -3,7 +3,7 @@
 if ["$1" = ""]; then
     echo "You need to input a valid path"
 else
-    cd $1
+    cd "$1
     for homework in tarea*.pdf
     do
         touch $(echo $homework | sed 's/.pdf/-comentarios.md/')
