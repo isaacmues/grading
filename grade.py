f155386f8dd8fef93194bf9eb60e5d66cdb7e741
Isaac <isaacmues@gmail.com>
2023-09-13 21:08:48 -0600

    El script en Python es capaz de replicar el título de la plantilla original

--- grade.py
+++ grade.py
@@ -1,14 +1,21 @@
 from pylatex import Document, Section, Command, TextColor
-from pylatex.utils import NoEscape
+from pylatex.utils import NoEscape, bold
 import yaml
 
 
 class Calificacion(Document):
 
-    def __init__(self, titulo):
+    def __init__(self, datos):
         super().__init__()
 
-        self.preamble.append(Command('title', titulo))
+        self.estudiante = datos["nombre"]
+        self.materia = datos["materia"]
+        self.trabajo = ' '.join((datos["tipo"].capitalize(), str(datos["numero"])))
+        self.comentarios = [datos["puntuacion"][k]["comentario"] for k in datos["puntuacion"]]
+        self.puntos = [datos["puntuacion"][k]["puntos"] for k in datos["puntuacion"]]
+        self.calificacion = self.calcula_calificacion()
+
+        self.preamble.append(Command('title', self.crea_titulo()))
         self.preamble.append(Command('author', ''))
         self.preamble.append(Command('date', ''))
         self.append(NoEscape(r'\maketitle'))
@@ -17,12 +24,16 @@ class Calificacion(Document):
         with self.create(Section('Comentarios')):
             self.append('Aquí irían los comentarios')
 
+    def calcula_calificacion(self):
+        return round(sum(self.puntos) / len(self.puntos) * 100)
 
-def titulo(materia, estudiante):
+    def crea_titulo(self):
+        materia = ''.join((r'\Large{\textsc{', self.materia, r'}}'))
+        trabajo = ''.join((r'\huge{', self.trabajo, ' de ', self.estudiante, r'}'))
+        calificacion = bold(''.join((r'\Large{Calificación: ', str(self.calificacion), r' de 100}')), escape=False)
 
-    materia_sc = ''.join((r'\Large{\textsc{', materia, r'}}'))
+        return NoEscape(r' \\ '.join((materia, trabajo, calificacion)))
 
-    return NoEscape(r' \\ '.join((materia_sc, estudiante)))
 
 if __name__ == '__main__':
 
@@ -30,12 +41,10 @@ if __name__ == '__main__':
     with open('test.yml', 'r') as file:
         datos = yaml.safe_load(file)
 
-    print(datos["nombre"])
-
-    doc = Calificacion(
-            titulo('Hermenéutica', datos["nombre"])
-            )
+    doc = Calificacion(datos)
     doc.fill_document()
+
+    print(doc.trabajo)
 
     doc.generate_pdf('test', clean_tex=False)
     tex = doc.dumps()
